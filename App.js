1828c717dd80722f28ac5288a9fcbcaaf1234531
Shyma-B <shymaprosad.edu@gmail.com>
2024-04-26 08:59:36 +0530

    episode-04 food ordering app initial

--- App.js
+++ App.js
@@ -1,51 +1,401 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => object => (render) => HTMLElement
-// const heading = React.createElement("h1", { id: "heading" }, "Namaste React ");
 
-// jsx element
-// const title = (
-//   <h1 id="heading" className="head">Namaste React from jsx element</h1>
-// );
+const Header = () => {
+  return (
+  <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP-Stp3j9l7A7FmeODEQvEwQcBTz_-55i1uZrCXG6lyA&s"></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+  </div>
+  );
+};
 
-const TitleComponent = () => (
-  <h1 id="heading" className="head">Namaste React from jsx</h1>
-);
+const resList = [
+  
+  {
+    "info": {
+      "id": "112767",
+      "name": "Tea Junction",
+      "cloudinaryImageId": "77d2fffe27cd59af5045a102d08af522",
+      "locality": "Lake Road",
+      "areaName": "Ballygunge",
+      "costForTwo": "₹150 for two",
+      "cuisines": [
+        "Beverages",
+        "Snacks",
+        "Desserts"
+      ],
+      "avgRating": 4.5,
+      "parentId": "4311",
+      "avgRatingString": "4.5",
+      "totalRatingsString": "5K+",
+      "sla": {
+        "deliveryTime": 29,
+        "lastMileTravel": 3,
+        "serviceability": "SERVICEABLE",
+        "slaString": "25-30 mins",
+        "lastMileTravelString": "3.0 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-04-26 23:59:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "50% OFF",
+        "subHeader": "UPTO ₹100"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      }
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/restaurants/tea-junction-lake-road-ballygunge-kolkata-112767",
+      "type": "WEBLINK"
+    }
+  },
+  {
+    "info": {
+      "id": "752986",
+      "name": "GetAWay-Ice Creams & Desserts",
+      "cloudinaryImageId": "8194c8fed09497bce3714d2586653545",
+      "locality": "Minapara road",
+      "areaName": "kolkata municipal corporation",
+      "costForTwo": "₹250 for two",
+      "cuisines": [
+        "Desserts",
+        "Ice Cream",
+        "Healthy Food"
+      ],
+      "avgRating": 4.3,
+      "veg": true,
+      "parentId": "354819",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "50+",
+      "sla": {
+        "deliveryTime": 32,
+        "lastMileTravel": 5.9,
+        "serviceability": "SERVICEABLE",
+        "slaString": "30-35 mins",
+        "lastMileTravelString": "5.9 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-05-03 00:00:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "50% OFF",
+        "subHeader": "UPTO ₹100"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      }
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/restaurants/getaway-ice-creams-and-desserts-minapara-road-kolkata-municipal-corporation-kolkata-752986",
+      "type": "WEBLINK"
+    }
+  },
+  {
+    "info": {
+      "id": "8912",
+      "name": "Subway",
+      "cloudinaryImageId": "63178e3e64d503a479f2a2048a474552",
+      "locality": "Park Street",
+      "areaName": "Central Kolkata",
+      "costForTwo": "₹350 for two",
+      "cuisines": [
+        "Healthy Food",
+        "Salads",
+        "Snacks",
+        "Desserts",
+        "Beverages"
+      ],
+      "avgRating": 4.3,
+      "parentId": "2",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "10K+",
+      "sla": {
+        "deliveryTime": 42,
+        "lastMileTravel": 6.2,
+        "serviceability": "SERVICEABLE",
+        "slaString": "40-45 mins",
+        "lastMileTravelString": "6.2 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-04-26 07:59:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "aggregatedDiscountInfoV2": {
+        
+      },
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      }
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/restaurants/subway-park-street-central-kolkata-kolkata-8912",
+      "type": "WEBLINK"
+    }
+  },
+  {
+    "info": {
+      "id": "19117",
+      "name": "Cafe Coffee Day",
+      "cloudinaryImageId": "b70c7d23d197251b7b315b7e4d8173ae",
+      "locality": "Joka",
+      "areaName": "South Kolkata",
+      "costForTwo": "₹400 for two",
+      "cuisines": [
+        "Beverages",
+        "Cafe",
+        "Snacks",
+        "Desserts",
+        "Burgers",
+        "Ice Cream",
+        "Bakery",
+        "Fast Food"
+      ],
+      "avgRating": 4.4,
+      "parentId": "1",
+      "avgRatingString": "4.4",
+      "totalRatingsString": "1K+",
+      "sla": {
+        "deliveryTime": 21,
+        "lastMileTravel": 2.2,
+        "serviceability": "SERVICEABLE",
+        "slaString": "20-25 mins",
+        "lastMileTravelString": "2.2 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-05-03 00:00:00",
+        "opened": true
+      },
+      "badges": {
+        "imageBadges": [
+          {
+            "imageId": "Rxawards/_CATEGORY-Cafe%20&%20Chai.png",
+            "description": "Delivery!"
+          }
+        ]
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            "badgeObject": [
+              {
+                "attributes": {
+                  "description": "Delivery!",
+                  "imageId": "Rxawards/_CATEGORY-Cafe%20&%20Chai.png"
+                }
+              }
+            ]
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "50% OFF",
+        "subHeader": "UPTO ₹100"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      }
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/restaurants/cafe-coffee-day-joka-south-kolkata-kolkata-19117",
+      "type": "WEBLINK"
+    }
+  }
+];
 
-
-// const HeadingComponentNormal = function () {
-//   return (
-//   <div id="container">
-//     <TitleComponent />
-//     <h1> Namaste React Funtional Component </h1>
-//   </div>
-// )
-// };
-
-//passing jsx component into component i.e Component composition
-const HeadingComponent = () => (
-    <div id="container">
-      <TitleComponent />
-      {TitleComponent()}
-      <h1> Namaste React Funtional Component </h1>
+const ResturantCard = (props) => {
+  const { resData } = props;
+  const { cloudinaryImageId,  name, cuisines, avgRating, costForTwo} = resData?.info;
+  return (
+    <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
+      <img alt="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +cloudinaryImageId} className="res-logo"></img>
+      <h3> {name} </h3>
+      <h4> {cuisines.join(",")} stars</h4>
+      <h4> {avgRating} stars</h4>
+      <h4> {costForTwo} </h4>
+      <h4> {resData.info.sla.deliveryTime} minutes</h4>
     </div>
   );
+}
 
-// passing jsx element
-// const HeadingComponent = () => (
-//     <div id="container">
-//       {title}
-//       <h1> Namaste React Funtional Component </h1>
-//     </div>
-//   );
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">search</div>
+      <div className="res-container">
+        {
+          resList.map((resturant) => <ResturantCard key={resturant.info.id} resData={resturant}/>)  
+        }
+      </div>
+    </div>
+  );
+}
 
-const HeadingComponent2 = () => <h1> Namaste React Funtional Component </h1>
+const AppLayout = () => {
+  return (
+  <div className="app">
+    <Header />
+    <Body />
+  </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
-// root.render(jsxHeading);
-root.render(<HeadingComponent />);
-
-
+root.render(<AppLayout />);
